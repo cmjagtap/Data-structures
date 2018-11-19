96117dedd95c1c244cabb2fa922122126b817391
Chandramohan Jagtap <cmjagtap1@gmail.com>
2018-11-19 18:29:44 +0530

    New

---
+++ Array/findMinmum_rotatedArray.c
@@ -0,0 +1,37 @@
+//find minimum element in sorted rotated array?
+// we use binary search by modifing to find it in log(n)
+#include<stdio.h>
+#include<stdlib.h>
+int findMinimumBinarySearch(int *array,int start,int end)
+{
+  if(start==end)//only one element
+    {
+      return array[start];
+    }
+  int mid=start + (end-start)/2;
+  if(array[start]>array[mid] && array[mid]<array[mid+1]) 
+    {
+      return findMinimumBinarySearch(array,start,mid);
+    }
+  if(array[mid]>array[end])
+    {
+      return findMinimumBinarySearch(array,mid+1,end);
+    }
+  if(end<start)
+    {
+      return array[end];
+    }
+  return array[start];
+}
+int main()
+{
+  int *array,arraySize;
+  scanf("%d",&arraySize);
+  array=malloc(sizeof(int)*arraySize);
+  for(int i=0;i<arraySize;i++)
+    {
+      scanf("%d",&array[i]);
+    }
+  printf("Min %d\n",findMinimumBinarySearch(array,0,arraySize-1));
+  return 0;
+}
